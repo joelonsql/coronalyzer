fd2fdc9da37f8dee46477ba6dcd1176bebe91fe5
Joel Jakobsson <joel@compiler.org>
2020-04-03 11:31:08 +0200

    Add Confirmed cases in addition to Deaths

--- app.R
+++ app.R
@@ -4,24 +4,45 @@ library(drc)
 library(lubridate)
 library(scales) 
 
-country_deaths <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv") %>%
+confirmed_global <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv") %>%
     rename(province = "Province/State",
            country = "Country/Region") %>%
-    pivot_longer(-c(province,country,Lat,Long), names_to = "date", values_to = "deaths") %>%
+    pivot_longer(-c(province,country,Lat,Long), names_to = "date", values_to = "cases") %>%
     mutate(date = mdy(date)) %>%
     group_by(country, date) %>%
-    summarise(deaths = sum(deaths)) %>%
-    ungroup() %>%
-    filter(country != "Sweden")
-# Data for Sweden added manually from Folkhälsomyndigheten's dashboard:
+    summarise(cases = sum(cases)) %>%
+    ungroup()
+
+deaths_global <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv") %>%
+    rename(province = "Province/State",
+           country = "Country/Region") %>%
+    pivot_longer(-c(province,country,Lat,Long), names_to = "date", values_to = "cases") %>%
+    mutate(date = mdy(date)) %>%
+    group_by(country, date) %>%
+    summarise(cases = sum(cases)) %>%
+    ungroup()
+
+# Additional data for Sweden added manually from Folkhälsomyndighet's two sources:
+
+# FHM dashboard:
 # https://experience.arcgis.com/experience/09f821667ce64bf7be6f9f87457ed9aa
 fhm <- data.frame(
-    country     = "Sweden",
-    deaths      = c(1,1,1,2,3,7,8,10,12,16,20,23,33,36,42,66,92,102,110,146,180,239,282)
+    country     = "Sweden FHM Arcgis",
+    cases      = c(1,1,1,2,3,7,8,10,12,16,20,23,33,36,42,66,92,102,110,146,180,239,282)
 )
-fhm$date <- as.Date("2020-03-10") + 1:length(fhm$deaths)
-country_deaths <- rbind(fhm, country_deaths)
-countries <- unique(country_deaths$country)
+fhm$date <- as.Date("2020-03-10") + 1:length(fhm$cases)
+deaths_global <- rbind(fhm, deaths_global)
+
+# FHM Excel:
+# https://www.arcgis.com/sharing/rest/content/items/b5e7488e117749c19881cce45db13f7e/data
+fhm <- data.frame(
+    country     = "Sweden FHM Excel",
+    cases      = cumsum(c(1,1,2,2,1,6,7,9,8,11,8,16,22,27,31,26,25,26,26,13,5+9))
+)
+fhm$date <- as.Date("2020-03-12") + 1:length(fhm$cases)
+deaths_global <- rbind(fhm, deaths_global)
+
+countries <- unique(deaths_global$country)
 populations <- read_csv("populations.csv", col_names=c("country","population"))
 
 ui <- dashboardPage(
@@ -33,6 +54,7 @@ ui <- dashboardPage(
                     min = as.Date("2020-01-22"),
                     max = Sys.Date(),
                     value = Sys.Date()),
+        radioButtons("yaxis","Y-axis:", c("Deaths","Confirmed cases")),
         radioButtons("scale","Scale:", c("Linear"="lin","Logarithmic"="log"))
     ),
     dashboardBody(
@@ -41,12 +63,12 @@ ui <- dashboardPage(
                 plotOutput("graph")
             ),
             box(
-                plotOutput("graphRecentTotalDeaths")
+                plotOutput("graphRecentTotalCases")
             ),
             box(
-                plotOutput("graphRecentNewDeaths")
+                plotOutput("graphRecentNewCases")
             ),
-            infoBoxOutput("maxDeathsBox"),
+            infoBoxOutput("maxCasesBox"),
             box(
                 verbatimTextOutput("modelSummaryBox")
             )
@@ -55,38 +77,45 @@ ui <- dashboardPage(
 )
 
 server <- function(input, output, session) {
-    maxDeaths <- reactiveVal()
+    maxCases <- reactiveVal()
     summaryVal <- reactiveVal()
     graphDataVal <- reactiveVal()
     
     output$graph <- renderPlot({
-        
-        data <- country_deaths %>%
-            filter(country == input$country, deaths > 0)
-        first_death <- as.Date(min(data$date))
-        data$day <- as.integer(data$date - first_death + 1)
+
+        input_data <- NA
+        if (input$yaxis == "Deaths") {
+            input_data <- deaths_global
+        } else {
+            input_data <- confirmed_global
+        }
+
+        data <- input_data %>%
+            filter(country == input$country, cases > 0)
+        first_case <- as.Date(min(data$date))
+        data$day <- as.integer(data$date - first_case + 1)
         data$predictions <- NA
 
         updateSliderInput(session, "date", min = min(data$date))
         updateSliderInput(session, "date", max = max(data$date))
 
-        base_model <- drm(deaths ~ day, data = data, fct = LL.4(fixed=c(NA,0,NA,NA)))
-        x_limits <- c(first_death, max(
-            first_death+as.integer(base_model$coefficients["e:(Intercept)"])*2,
-            Sys.Date()+7
-        ))
+        base_model <- drm(cases ~ day, data = data, fct = LL.4(fixed=c(NA,0,NA,NA)))
 
         model_data <- filter(data, date <= input$date)
         
-        model <- drm(deaths ~ day, data = model_data, fct = LL.4(fixed=c(NA,0,NA,NA)))
+        model <- drm(cases ~ day, data = model_data, fct = LL.4(fixed=c(NA,0,NA,NA)))
         model_summary <- summary(model)
         summaryVal(model_summary)
         steepness <- model$coefficients["b:(Intercept)"]
         deceased <- model$coefficients["d:(Intercept)"]
         y_limits <- c(1,round(max(deceased, base_model$coefficients["d:(Intercept)"])))
         inflection <- model$coefficients["e:(Intercept)"]
-        inflection_date <- first_death + as.integer(inflection) - 1
-        maxDeaths(round(deceased))
+        x_limits <- c(first_case, max(
+            first_case+as.integer(inflection)*2,
+            Sys.Date()+7
+        ))
+        inflection_date <- first_case + as.integer(inflection) - 1
+        maxCases(round(deceased))
         data$model <- NA
         predict_day <- max(model_data$day) + 1
         end_day <- max(as.integer(2*inflection), max(model_data$day) + 7)
@@ -94,28 +123,28 @@ server <- function(input, output, session) {
         data <- data %>% add_row(
             date = NA,
             day = predict_day:end_day,
-            deaths = sapply(predict_day:end_day, function(day) {
-                round(deceased - deceased/(1 + (day/inflection)^(-steepness)))
+            cases = sapply(predict_day:end_day, function(day) {
+                round(predict(model,data.frame(day=day))) # The formula is: round(deceased - deceased/(1 + (day/inflection)^(-steepness)))
             }),
             type = "Forecast"
         )
         # Convert day from integer to date
-        data$date <- first_death + data$day - 1
-        predict_day <- first_death + predict_day - 1
-        predict_total <- filter(data,date==predict_day & type == "Forecast")$deaths
-        predict_new <- predict_total - filter(data,date==predict_day-1 & type == "Forecast")$deaths
+        data$date <- first_case + data$day - 1
+        predict_day <- first_case + predict_day - 1
+        predict_total <- filter(data,date==predict_day & type == "Forecast")$cases
+        predict_new <- predict_total - filter(data,date==predict_day-1 & type == "Forecast")$cases
 
         graphDataVal(data %>% filter(date <= input$date | type == "Forecast"))
         
         plot <- ggplot(data, aes(x=date)) +
-            geom_point(aes(y=deaths, color=type, alpha=0.8)) +
+            geom_point(aes(y=cases, color=type, alpha=0.8)) +
             guides(alpha = FALSE) +
             theme_minimal() +
             scale_x_date(limits=x_limits) +
             geom_vline(aes(xintercept = inflection_date, color="Point of inflection")) +
             xlab("Datum") +
-            ylab("Deaths") +
-            ggtitle(paste0("COVID-19 - Total Deaths - ", input$country),
+            ylab(input$yaxis) +
+            ggtitle(paste0("COVID-19 - Total Cases - ", input$country),
                     subtitle = paste0("Forecast ", predict_day, " : ", predict_total, " total, ", predict_new, " new")
             )
 
@@ -127,19 +156,19 @@ server <- function(input, output, session) {
                     
     })
 
-    output$graphRecentTotalDeaths <- renderPlot({
+    output$graphRecentTotalCases <- renderPlot({
         
         data <- graphDataVal()
         
         plot <- ggplot(data %>%
                    subset(date >= (input$date-7) & date <= (input$date+7)), aes(x=date)) +
-            geom_col(aes(y=deaths, fill=type)) +
-            geom_text(aes(y=deaths, label = deaths),
+            geom_col(aes(y=cases, fill=type)) +
+            geom_text(aes(y=cases, label = cases),
                       show.legend = FALSE, check_overlap = TRUE) +
             theme_minimal() +
             xlab("Date") +
-            ylab("Total Deceased") +
-            ggtitle(paste0("COVID-19 - Total Deaths - 7 day forecast - ", input$country))
+            ylab(input$yaxis) +
+            ggtitle(paste0("COVID-19 - Total - 7 day forecast - ", input$country))
 
         if (input$scale == "log") {
             print(plot + scale_y_log10(labels = comma))
@@ -150,20 +179,20 @@ server <- function(input, output, session) {
                 
     })
     
-    output$graphRecentNewDeaths <- renderPlot({
+    output$graphRecentNewCases <- renderPlot({
 
         data <- graphDataVal()
         
         plot <- ggplot(data %>%
-                   mutate(new_deaths = c(0,diff(deaths))) %>%
+                   mutate(new_cases = c(0,diff(cases))) %>%
                    subset(date >= (input$date-7) & date <= (input$date+7)), aes(x=date)) +
-            geom_col(aes(y=new_deaths, fill=type)) +
-            geom_text(aes(y=new_deaths, label = new_deaths),
+            geom_col(aes(y=new_cases, fill=type)) +
+            geom_text(aes(y=new_cases, label = new_cases),
                       show.legend = FALSE, check_overlap = TRUE) +
             theme_minimal() +
             xlab("Date") +
-            ylab("New Deceased") +
-            ggtitle(paste0("COVID-19 - New Deaths - 7 day forecast - ", input$country))
+            ylab(input$yaxis) +
+            ggtitle(paste0("COVID-19 - New - 7 day forecast - ", input$country))
 
         if (input$scale == "log") {
             print(plot + scale_y_log10(labels = comma))
@@ -173,9 +202,9 @@ server <- function(input, output, session) {
         
     })
 
-    output$maxDeathsBox <- renderInfoBox({
+    output$maxCasesBox <- renderInfoBox({
         infoBox(
-            "FINAL DEATHS PREDICTED", maxDeaths(), icon = icon("skull"),
+            "FINAL CASES PREDICTED", maxCases(),
             color = "purple"
         )
     })
